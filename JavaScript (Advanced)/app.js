95550f2efebb5c8838d33dbbc9488ec4f0039add
user1 <user1@itechart-group.com>
2014-07-17 12:18:50 +0300

    merged tasks in javascript

---
+++ JavaScript (Advanced)/app.js
@@ -0,0 +1,97 @@
+﻿(function () {
+    var functionsCollect = new functionsLibrary();
+
+    var f = functionsCollect.partial(sum,  2);
+    var res = f(5, 6, 7);
+    console.log('partial: ' + res);
+
+    var cur = functionsCollect.curry(mul);
+    var res = cur(2)(3)(4);
+    console.log('curry: ' + res);
+
+    var foldArray = [1, 2, 3, 5];
+    var foldRes = functionsCollect.fold(foldArray, sumElements, 0);
+    console.log('folding: '  + foldRes);
+
+    var unfoldRes = functionsCollect.unfold(cube, 1);
+    console.log('unfolding:' + unfoldRes);
+
+    var mapArray = [1, 2, 3];
+    var mapRes = functionsCollect.map(mapArray, sqr);
+    console.log('map: ' + mapRes);
+
+    var arrayForFilter = [1, 2, 3, 0, -1, -2, -3, 6];
+    var evenNumbers = functionsCollect.filter(arrayForFilter, isEven);
+    console.log('filter: ' + evenNumbers);
+    /*7. average of even numbers*/
+    var averageEvenNumbers = functionsCollect.fold(evenNumbers, sumElements) / evenNumbers.length;
+    console.log('average of even numbers: ' + averageEvenNumbers);
+
+    /*8. summ of 10 random numbers*/
+    var randomArray = [];
+    for (var i = 0; i < 5; i++) {
+        var randomNumber = Math.floor(Math.random() * 20 - 10);
+        randomArray.push(randomNumber);
+    }
+    var sumRandomNumbers = functionsCollect.fold(randomArray, sumElements);
+    console.log('random array: ' + randomArray);
+    console.log('sum of random array: ' + sumRandomNumbers);
+
+    var arrayForFirstMethod = [1, -3, 4, 0, 3, 2, 5];
+    console.log('first of even: ' + functionsCollect.firstElement(arrayForFirstMethod, isEven));
+
+    var lazyRes = functionsCollect.lazy(sum, 4,5,6);
+    console.log('lazy: ' + lazyRes);
+    //console.log(lazyRes());
+    //lazyRes = functionsCollect.lazy(sum, [2, 8]);
+    //console.log(lazyRes());
+
+    var sqr = functionsCollect.memoisation(sqr);
+    console.log('memoization: ' + sqr(5));
+    //console.log(sqr(5));
+    //console.log(sqr(6));
+
+
+    /*functions*/
+
+    /*for filter*/
+    function isEven(value) {
+        return (value % 2 === 0)
+    }
+
+    /*for map*/
+    function sqr(value) {
+        return value * value;
+    }
+
+    /*for unfold*/
+    function cube(state) {
+        var resValue = Math.pow(state, 3);
+        var newState = state + 1 < 10 ? state + 1 : null;
+        var resMas = [newState, resValue];
+        return resMas;
+    }
+
+    /*for fold*/
+    function sumElements(previousValue, currentValue/*, index, array*/) {
+        return previousValue + currentValue;
+    }
+    
+    function mul(x, y, z) {
+        var m = 1;
+        for (var i = 0; i < arguments.length; i++) {
+            m = m * Number(arguments[i]);
+        }
+        return m;
+    }
+
+    function sum(sss) {
+        var s = 0;
+        for (var i = 0; i < arguments.length; i++) {
+            s += Number(arguments[i]);
+        }
+        console.log(s);
+        return s;
+    }
+}());
+
---
+++ JavaScript (Advanced)/functions.js
@@ -0,0 +1,171 @@
+﻿function functionsLibrary() {
+
+    function _partial(func) {
+        var args = Array.prototype.slice.apply(arguments).slice(1);
+
+        return function () {
+            return func.apply(this, Array.prototype.concat.apply(args, arguments));
+        }
+    }
+
+    function _curry() {
+        var fun;
+        var n;
+        var args;
+
+        if (typeof arguments[0] === 'function') {
+            fun = arguments[0];
+            args = Array.prototype.slice.apply(arguments).slice(1);
+            n = fun.length - args.length;
+        }
+        else {
+            n = arguments[0];
+            fun = arguments[1];
+            args = Array.prototype.slice.apply(arguments).slice(2);
+        }
+
+        return function () {
+            var newArgs = Array.prototype.slice.apply(arguments);
+            var allCurrentArgs = Array.prototype.concat.apply(args, newArgs);
+            var otherParamsCount = n - newArgs.length;
+
+            if (otherParamsCount) {
+                return _curry.apply(this, Array.prototype.concat(otherParamsCount, fun, allCurrentArgs));
+            }
+            else {
+                return fun.apply(this, allCurrentArgs);
+            }
+        }
+    }
+
+    function _fold(arr, callback, initialValue) {
+        var preValue, index;
+        if (initialValue) {
+            preValue = initialValue;
+            index = 0;
+        }
+        else {
+            preValue = arr[0];
+            index = 1;
+        }
+
+        for (var i = index; i < arr.length; i++) {
+            preValue = callback(preValue, arr[i]);
+        }
+
+        return preValue;
+    }
+
+    function _unfold(callback, initialValue) {
+        var state = initialValue;
+        var newValue = null;
+        var array = [];
+
+        while (state) {
+            if (newValue != null) {
+                array.push(newValue);
+            }
+            var resMas = callback(state);
+            state = resMas[0];
+            newValue = resMas[1];
+        }
+
+        return array;
+    }
+
+    function _map(array, callback) {
+        for (var i = 0; i < array.length; i++) {
+            array[i] = callback(array[i]);
+        }
+        return array;
+    }
+
+    function _filter(array, callback) {
+        var resMas = [];
+        for (var i = 0; i < array.length; i++) {
+            var elem = array[i];
+            if (callback(elem)) {
+                resMas.push(elem);
+            }
+        }
+        return resMas;
+    }
+
+    function _firstElement(array, callback) {
+        return _filter(array, callback)[0];
+    }
+
+    function _lazy() {       
+        var args;
+        var func;
+        
+        if (typeof arguments[0] === 'function') {
+            console.log('sfsdf');
+            func = arguments[0];
+            args = Array.prototype.slice.apply(arguments).slice(1);
+        } else {
+            args = Array.prototype.slice.apply(arguments);
+        }
+
+        return function () {
+            console.log('gj');
+            var a = arguments[0];
+            if (typeof a === 'function') {
+
+                return _lazy.apply(this, args);
+                //  } else {
+                //       return func.apply(this, args);
+                //   }
+            } else {
+                console.log('else');
+                return func.apply(this, args);
+            }
+        };
+    }
+    
+
+    //function calc(a, b, c) {
+    //    console.log(a);
+    //    return a + b + c;
+    //}
+
+    //function lazy(f, args) { }
+
+    //var lazyCalc = lazy(calc, 1, 2, 3);
+
+    //lazyCalc();
+    //var lazyCalc = lazy(_memoisation, _lazy);
+
+    function _memoisation(func) {
+        var cache = [];
+
+        return function() {
+            var res;
+            var arg = arguments[0];
+            if (typeof arg !== 'function') {
+                if (arg in cache) {
+                    res = cache[arg];
+                    //console.log('from cache');
+                } else {
+                    res = func.call(this, arg);
+                    cache[arg] = res;
+                    //console.log('not from cache');
+                }
+            }
+
+            return res;
+        };
+    }
+
+    return {
+        partial: _partial,
+        curry: _curry,
+        fold: _fold,
+        unfold: _unfold,
+        map: _map,
+        filter: _filter,
+        firstElement: _firstElement,
+        lazy: _lazy,
+        memoisation: _memoisation
+    };
+};
